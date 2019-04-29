59999e3618dba27d939f4651a72eacd91b96dfac
Michael Larragueta and Reese Lee <reese@>
2019-04-29 15:25:48 -0700

    Add everything

--- ToDoList/ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList/ToDoList.Tests/ModelTests/ItemTests.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ToDoList.Models;
+using ToDoList;
 using System;
 using System.Collections.Generic;
 
---
+++ ToDoList/ToDoList/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoList.Controllers
+{
+    public class Home1 : Controller
+    {
+
+      [HttpGet("/")]
+      public ActionResult Index()
+      {
+        return View(0);
+      }
+
+    }
+}
